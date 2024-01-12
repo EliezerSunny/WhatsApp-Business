1ac2c233abe068dce42157899c42b0f5d2b96ff5
Adetunjieliazer <adetunjieliazer@gmail.com>
2024-01-12 03:35:19 +0100

    WhatsApp-Business

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,94 @@
-function headMenuList() {
-	var menuBox = document.getElementById('head_menu_list');
 
-	if (menuBox.style.display == "none") {
-		menuBox.style.display = "block";
-	} else {
-		menuBox.style.display = "none";
-	}
+
+
+
+
+// function headMenuList() {
+// 	var menuBox = document.getElementById('head_menu_list');
+
+// 	if (menuBox.style.display == "none") {
+// 		menuBox.style.display = "block";
+// 	} else {
+// 		menuBox.style.display = "none";
+// 	}
+// }
+
+
+
+// function search() {
+// 	var menuBox = document.getElementById('search');
+
+// 	if (menuBox.style.display == "none") {
+// 		menuBox.style.display = "block";
+// 	} else {
+// 		menuBox.style.display = "none";
+// 	}
+// }
+
+
+// function camera() {
+// 	var menuBox = document.getElementById('camera');
+
+// 	if (menuBox.style.display == "none") {
+// 		menuBox.style.display = "block";
+// 	} else {
+// 		menuBox.style.display = "none";
+// 	}
+// }
+
+
+
+
+// $("#toggle").on("click", function() {
+// 	$(".istoggable").toggle();
+// });
+
+
+const $menu = $('.head11')
+
+const onMouseUp = e => {
+ if (!$menu.is(e.target) // If the target of the click isn't the container...
+   && $menu.has(e.target).length === 0) // ... or a descendant of the container.
+   {
+     $menu.removeClass('is-active')
+  }
 }
 
+//
+$('.toggle').on('click', () => {
+	$menu.toggleClass('is-active').promise().done(() => {
+	  if ($menu.hasClass('is-active')) {
+		$(document).on('mouseup', onMouseUp) // Only listen for mouseup when menu is active...
+	  } else {
+		$(document).off('mouseup', onMouseUp) // else remove listener.
+	  }
+	})
+  })
+//
 
 
-function search() {
-	var menuBox = document.getElementById('search');
+//
+$('.toggle2').on('click', () => {
+	$menu.toggleClass('is-active').promise().done(() => {
+	  if ($menu.hasClass('is-active')) {
+		$(document).on('mouseup', onMouseUp) // Only listen for mouseup when menu is active...
+	  } else {
+		$(document).off('mouseup', onMouseUp) // else remove listener.
+	  }
+	})
+  })
+//
 
-	if (menuBox.style.display == "none") {
-		menuBox.style.display = "block";
-	} else {
-		menuBox.style.display = "none";
-	}
-}
 
-
-function camera() {
-	var menuBox = document.getElementById('camera');
-
-	if (menuBox.style.display == "none") {
-		menuBox.style.display = "block";
-	} else {
-		menuBox.style.display = "none";
-	}
-}
+$('.toggle3').on('click', () => {
+  $menu.toggleClass('is-active').promise().done(() => {
+    if ($menu.hasClass('is-active')) {
+      $(document).on('mouseup', onMouseUp) // Only listen for mouseup when menu is active...
+    } else {
+      $(document).off('mouseup', onMouseUp) // else remove listener.
+    }
+  })
+})
 
 
 
